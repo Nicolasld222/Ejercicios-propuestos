e183b794c92aedd2f9ed77ede73c5218df19bfd9
Nicolas Londoño Diaz <nicolaslondia@gmail.com>
2024-08-26 11:37:58 -0500

    Update Banco.js

--- Ejercicios/Banco.js
+++ Ejercicios/Banco.js
@@ -82,3 +82,62 @@ function Menu(validarIdentificacion, ValidarPin){
     
     if(validarIdentificacion === )
 }
+
+
+//OTRO
+
+// Array of users
+const usuarios = [
+    {id:109022, Nombre:"Camilo", Pin: 1234, saldo:100000},
+    {id:109023, Nombre:"Carlos", Pin: 1235, saldo:100000},
+    {id:109024, Nombre:"Yarleno", Pin: 1236, saldo:100000}
+
+];
+
+//El usuario se identifica
+function idIngresado(){
+    return Number(prompt("Ponga su numero de identificacion"))
+}
+idIngresado();
+
+//El usuario pone el pin
+function PinIngresado(){
+    return Number(prompt("Ingrese su pin"))
+}
+PinIngresado();
+
+
+//Funcion Si es verdadera  la informacion
+
+function validarIdentificacion(idIngresado, PinIngresado) {
+
+    const usuarioEncontrado = usuarios.find(usuarios => usuarios.id == idingresadaUsuario)
+    const contraseñaEncontrada = usuarios.find(usuarios => usuarios.Pin)
+
+    if(usuarioEncontrado === idIngresado){
+        (contraseñaEncontrada === PinIngresado)
+        alert("Su usuario es correcto")
+        return true
+    }else{
+        alert("Su usuario es incorrecto")
+        return false
+    }
+}
+
+validarIdentificacion();
+
+//Menu 
+function Menu(validarIdentificacion){
+    
+    while(validarIdentificacion === true){
+        let opcion = Number(prompt("Bienvenido, seleccione 1)Consultar saldo 2)retirar 3)Transferir"))
+        
+        switch(opcion){
+            case 1: return saldo
+            case 2: return Retiro 
+            case 3: return Transferencia
+        }
+    }
+
+}
+
